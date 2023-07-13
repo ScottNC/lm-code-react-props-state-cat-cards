124183ab3b3d29ac7ff069eeae7949a50b271d78
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-07-13 23:44:29 +0100

    destructures props

--- src/components/cat_image.tsx
+++ src/components/cat_image.tsx
@@ -7,14 +7,14 @@ interface CatImageProps {
 	attributionUrl?: string;
 }
 
-const CatImage: React.FC<CatImageProps> = (props) => (
+const CatImage: React.FC<CatImageProps> = ({image, altText, licenceType, licenceUrl, attributionName, attributionUrl}) => (
 	<>
-		<img className='card__image' src={props.image} alt={props.altText} />
+		<img className='card__image' src={image} alt={altText} />
     <p className='card__text__small'>
-			Image licenced under <a href={props.licenceUrl}>{props.licenceType}</a>
-			{props.attributionName && (
+			Image licenced under <a href={licenceUrl}>{licenceType}</a>
+			{attributionName && (
 				<>
-					&nbsp;by <a href={props.attributionUrl}>{props.attributionName}</a>
+					&nbsp;by <a href={attributionUrl}>{attributionName}</a>
 				</>
 			)}
 		</p>
